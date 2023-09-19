575330c6d850bcd9285cbcd3378205b1207add50
Walid-Mahmoudi <145464003+Walid-Mahmoudi@users.noreply.github.com>
2023-09-19 16:51:00 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+let action = document.getElementById('action');
+let drop = document.getElementById('dropdwn');
+
+drop.addEventListener( 'click' , function()
+{
+    action.classList.toggle('hide'); 
+})
